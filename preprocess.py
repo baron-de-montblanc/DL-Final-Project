de61f425f681b5d2a02e9b86d724afb39755c52f
baron-de-montblanc <jade_ducharme@brown.edu>
2024-05-01 20:52:30 -0400

    made some major changes to the preprocessing pipeline, we now have seven features to work with instead of four

--- preprocess.py
+++ preprocess.py
@@ -71,14 +71,127 @@ def get_data(filename, attribute, verbose=False):
 
 
 
+def constituent_preprocess(data, features, max_constits=None):
+    """ constituent - This function applies a standard preprocessing to the
+    jet data contained in data. It will operate on the raw constituent
+    level quantities and return 7 constituent level quantities which can be
+    used for tagger training.
+
+    Arguments:
+    data (np array) - Array of constituent-level data
+    features (list) - List of constituent-level feature names
+    # max_constits (int) - The maximum number of constituents to consider in
+    # preprocessing. Cut jet constituents at this number.
+
+    Returns:
+    (np array) - The seven constituent level quantities, stacked along the last
+    axis.
+
+    ADAPTED FROM ATLAS-top-tagging-open-data/preprocessing.py ON GITHUB:
+    https://gitlab.cern.ch/atlas/ATLAS-top-tagging-open-data/-/blob/master/preprocessing.py?ref_type=heads
+    """
+
+    ############################## Load Data ###################################
+
+    # Pull data from data dict
+    pt = data[:,np.where(features=='fjet_clus_pt')[0][0],:]
+    eta = data[:,np.where(features=='fjet_clus_eta')[0][0],:]
+    phi = data[:,np.where(features=='fjet_clus_phi')[0][0],:]
+    energy = data[:,np.where(features=='fjet_clus_E')[0][0],:]
+
+    # Find location of zero pt entries in each jet. This will be used as a
+    # mask to re-zero out entries after all preprocessing steps
+    mask = np.asarray(pt == 0).nonzero()
+
+    ########################## Angular Coordinates #############################
+
+    # 1. Center hardest constituent in eta/phi plane. First find eta and
+    # phi shifts to be applied
+    eta_shift = eta[:,0]
+    phi_shift = phi[:,0]
+
+    # Apply them using np.newaxis
+    eta_center = eta - eta_shift[:,np.newaxis]
+    phi_center = phi - phi_shift[:,np.newaxis]
+
+    # Fix discontinuity in phi at +/- pi using np.where
+    phi_center = np.where(phi_center > np.pi, phi_center - 2*np.pi, phi_center)
+    phi_center = np.where(phi_center < -np.pi, phi_center + 2*np.pi, phi_center)
+
+    # 2. Rotate such that 2nd hardest constituent sits on negative phi axis
+    second_eta = eta_center[:,1]
+    second_phi = phi_center[:,1]
+    alpha = np.arctan2(second_phi, second_eta) + np.pi/2
+    eta_rot = (eta_center * np.cos(alpha[:,np.newaxis]) +
+               phi_center * np.sin(alpha[:,np.newaxis]))
+    phi_rot = (-eta_center * np.sin(alpha[:,np.newaxis]) +
+               phi_center * np.cos(alpha[:,np.newaxis]))
+
+    # 3. If needed, reflect so 3rd hardest constituent is in positive eta
+    third_eta = eta_rot[:,2]
+    parity = np.where(third_eta < 0, -1, 1)
+    eta_flip = (eta_rot * parity[:,np.newaxis]).astype(np.float32)
+    # Cast to float32 needed to keep numpy from turning eta to double precision
+
+    # 4. Calculate R with pre-processed eta/phi
+    radius = np.sqrt(eta_flip ** 2 + phi_rot ** 2)
+
+    ############################# pT and Energy ################################
+
+    # Take the logarithm, ignoring -infs which will be set to zero later
+    log_pt = np.log(pt)
+    log_energy = np.log(energy)
+
+    # Sum pt and energy in each jet
+    sum_pt = np.sum(pt, axis=1)
+    sum_energy = np.sum(energy, axis=1)
+
+    # Normalize pt and energy and again take logarithm
+    lognorm_pt = np.log(pt / sum_pt[:,np.newaxis])
+    lognorm_energy = np.log(energy / sum_energy[:,np.newaxis])
+
+    ########################### Finalize and Return ############################
+
+    # Reset all of the original zero entries to zero
+    eta_flip[mask] = 0
+    phi_rot[mask] = 0
+    log_pt[mask] = 0
+    log_energy[mask] = 0
+    lognorm_pt[mask] = 0
+    lognorm_energy[mask] = 0
+    radius[mask] = 0
+
+    # Stack along last axis
+    features = [eta_flip, phi_rot, log_pt, log_energy,
+                lognorm_pt, lognorm_energy, radius]
+    stacked_data = np.stack(features, axis=-1)
+
+    # Also return feature names
+    feature_names = [
+        "delta_eta",
+        "delta_phi",
+        "log_pt",
+        "log_E",
+        "lognorm_pt",
+        "lognorm_E",
+        "R"
+    ]
+
+    return stacked_data, np.array(feature_names)
+
+
+
+
+
+
+
 # --------------------- FCNN-specific functions -------------------------------------------
 
 
-def preprocess_split(data, labels, weights, train_split, val_split, seed=None):
+def standardize_split(data, labels, weights, train_split, val_split, seed=None):
     """
-    Given data of shape [INPUT_SIZE, NUM_FEATURES], labels of shape [INPUT_SIZE], and weights of shape [INPUT_SIZE]
-    Apply basic preprocessing steps:
-    - standardize
+    Given data of shape [INPUT_SIZE, NUM_FEATURES, NUM_CONSTITUENTS], labels of shape [INPUT_SIZE], and weights of shape [INPUT_SIZE]
+
     - cast to PyTorch TensorDataset
     - split into training, validation, and testing
 
@@ -93,10 +206,13 @@ def preprocess_split(data, labels, weights, train_split, val_split, seed=None):
     # Standardization (mean=0, std=1)
     mean = torch.mean(data, dim=0)
     std = torch.std(data, dim=0)
-    data_standardized = (data - mean) / std
+    data = (data - mean) / std
+
+    # get rid of NaNs and infs --> cast to zeros
+    data = torch.nan_to_num(data, nan=0.0, posinf=0.0, neginf=0.0)
 
     # Wrap into a TensorDataset with weights
-    dataset = TensorDataset(data_standardized, labels, weights)
+    dataset = TensorDataset(data, labels, weights)
 
     # Split into train, validation, and test sets
     total_length = len(dataset)
@@ -152,12 +268,10 @@ def prepare_graphs(data, labels, weights, k, device, batch_size=64):
 
     graphs = []
     for data, label, weight in tqdm(loader):
-        x = data.transpose(1, 2)  # Ensure data is in the correct shape (N, C, num_features)
-
         # Construct graphs for each sample in the batch
-        for i in range(x.size(0)):
-            edge_index = knn_graph(x[i], k=k, loop=False)
-            graph = WeightedData(weight=weight[i], x=x[i], edge_index=edge_index, y=label[i].unsqueeze(0))
+        for i in range(data.size(0)):
+            edge_index = knn_graph(data[i], k=k, loop=False)
+            graph = WeightedData(weight=weight[i], x=data[i], edge_index=edge_index, y=label[i].unsqueeze(0))
             graphs.append(graph.to("cpu"))  # for memory management, don't store these on GPU
 
     return graphs
